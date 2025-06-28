9355534845a278b98a3f65964b8d4bbabcc08650
Gabriel Fernando <gf313074@gmail.com>
2025-06-28 16:22:26 -0300

    create error handling to register user

--- src/http/controllers/register.ts
+++ src/http/controllers/register.ts
@@ -2,6 +2,7 @@ import { type FastifyReply, type FastifyRequest } from 'fastify';
 import { z } from 'zod';
 
 import { PrismaUsersRepository } from '@/repositories/prisma/prisma-users-repository.js';
+import { UserAlreadyExistsError } from '@/use-cases/errors/user-already-exists-error.js';
 import { RegisterUseCase } from '@/use-cases/register.js';
 
 export async function register(request: FastifyRequest, reply: FastifyReply) {
@@ -18,8 +19,10 @@ export async function register(request: FastifyRequest, reply: FastifyReply) {
     const registerUseCase = new RegisterUseCase(prismaUsersRepository);
 
     await registerUseCase.execute({ email, name, password });
-  } catch {
-    return reply.status(409).send();
+  } catch (error) {
+    if (error instanceof UserAlreadyExistsError) {
+      return reply.status(409).send({ message: error.message });
+    }
   }
 
   return reply.status(201).send();
