7a3674bc580cf40c081f557e51c8ff512476ecc0
Gabriel Fernando <gf313074@gmail.com>
2025-07-01 22:40:26 -0300

    create mock to fake date

--- src/repositories/in-memory/in-memory-check-ins-repository.ts
+++ src/repositories/in-memory/in-memory-check-ins-repository.ts
@@ -7,12 +7,24 @@ import type { CheckInsRepository } from '@/repositories/check-ins-repository.js'
 export class InMemoryCheckInsRepository implements CheckInsRepository {
   public items: CheckIn[] = [];
 
+  async findByUserIdOnDate(userId: string, date: Date) {
+    const checkInOnSameDate = this.items.find(
+      checkIn => checkIn.user_id === userId,
+    );
+
+    if (!checkInOnSameDate) {
+      return null;
+    }
+
+    return checkInOnSameDate;
+  }
+
   async create(data: Prisma.CheckInUncheckedCreateInput) {
     const checkIn = {
       id: randomUUID(),
       user_id: data.user_id,
       gym_id: data.gym_id,
-      // eslint-disable-next-line @typescript-eslint/no-unsafe-argument
+
       validated_at: data.validated_at ? new Date(data.validated_at) : null,
       created_at: new Date(),
     };
