6befc150517b83aca850323d1cbc96fa5344cfb0
Gabriel Fernando <gf313074@gmail.com>
2025-07-06 15:52:27 -0300

    create fetch nearby gyms use case and test

--- src/repositories/in-memory/in-memory-gyms-repository.ts
+++ src/repositories/in-memory/in-memory-gyms-repository.ts
@@ -3,7 +3,11 @@ import { randomUUID } from 'node:crypto';
 
 import { type Gym, Prisma } from '@prisma/client';
 
-import type { GymsRepository } from '@/repositories/gyms-repository.js';
+import type {
+  FindManyNearbyParams,
+  GymsRepository,
+} from '@/repositories/gyms-repository.js';
+import { getDistanceBetweenCoordinates } from '@/utils/get-distance-between-coordinates.js';
 
 export class InMemoryGymsRepository implements GymsRepository {
   public items: Gym[] = [];
@@ -22,6 +26,20 @@ export class InMemoryGymsRepository implements GymsRepository {
     this.items.push(gym);
 
     return gym;
+  }
+
+  async findManyNearby(params: FindManyNearbyParams) {
+    return this.items.filter(item => {
+      const distance = getDistanceBetweenCoordinates(
+        { latitude: params.latitude, longitude: params.longitude },
+        {
+          latitude: item.latitude.toNumber(),
+          longitude: item.longitude.toNumber(),
+        },
+      );
+
+      return distance < 10;
+    });
   }
 
   async searchMany(query: string, page: number) {
