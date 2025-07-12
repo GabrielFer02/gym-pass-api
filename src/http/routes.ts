d968ceaaac007bc37843169f478444bfdf3967b6
Gabriel Fernando <gf313074@gmail.com>
2025-07-12 15:22:30 -0300

    create middleware to profile route

--- src/http/routes.ts
+++ src/http/routes.ts
@@ -3,10 +3,11 @@ import { type FastifyInstance } from 'fastify';
 import { authenticate } from './controllers/authenticate.js';
 import { profile } from './controllers/profile.js';
 import { register } from './controllers/register.js';
+import { verifyJwt } from './middlewares/verify-jwt.js';
 
 export function appRoutes(app: FastifyInstance) {
   app.post('/users', register);
   app.post('/sessions', authenticate);
 
-  app.get('/me', profile);
+  app.get('/me', { onRequest: [verifyJwt] }, profile);
 }
