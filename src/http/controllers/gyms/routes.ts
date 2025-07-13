e70e0d4790e74e535dd94a2fae411886e784cecb
Gabriel Fernando <gf313074@gmail.com>
2025-07-13 16:09:58 -0300

    create routes to search and nearby gyms

--- src/http/controllers/gyms/routes.ts
+++ src/http/controllers/gyms/routes.ts
@@ -2,6 +2,15 @@ import type { FastifyInstance } from 'fastify';
 
 import { verifyJwt } from '@/http/middlewares/verify-jwt.js';
 
+import { create } from './create.js';
+import { nearby } from './nearby.js';
+import { search } from './search.js';
+
 export function gymsRoutes(app: FastifyInstance) {
   app.addHook('onRequest', verifyJwt);
+
+  app.get('/gyms/search', search);
+  app.get('/gyms/nearby', nearby);
+
+  app.post('/gyms', create);
 }
