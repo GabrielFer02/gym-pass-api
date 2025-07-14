4f9dd5896b5c887ca7aa19794e47ff2f8f089713
Gabriel Fernando <gf313074@gmail.com>
2025-07-13 22:11:12 -0300

    create check-ins e2e tests

--- src/http/controllers/check-ins/create.spec.ts
+++ src/http/controllers/check-ins/create.spec.ts
@@ -2,9 +2,10 @@ import request from 'supertest';
 import { afterAll, beforeAll, describe, expect, it } from 'vitest';
 
 import { app } from '@/app.js';
+import { prisma } from '@/lib/prisma.js';
 import { createAndAuthenticateUser } from '@/utils/test/create-and-authenticate-user.js';
 
-describe('Profile (e2e)', () => {
+describe('Create Check-in (e2e)', () => {
   beforeAll(async () => {
     await app.ready();
   });
@@ -13,20 +14,26 @@ describe('Profile (e2e)', () => {
     await app.close();
   });
 
-  it('should be able to get user profile', async () => {
+  it('should be able to create a check-in', async () => {
     const { token } = await createAndAuthenticateUser(app);
 
-    const profileResponse = await request(app.server)
-      .get('/me')
+    const gym = await prisma.gym.create({
+      data: {
+        title: 'JavaScript Gym',
+        latitude: -27.2092052,
+        longitude: -49.6401091,
+      },
+    });
+
+    const response = await request(app.server)
+      .post(`/gyms/${gym.id}/check-ins`)
       // eslint-disable-next-line @typescript-eslint/restrict-template-expressions
       .set('Authorization', `Bearer ${token}`)
-      .send();
+      .send({
+        latitude: -27.2092052,
+        longitude: -49.6401091,
+      });
 
-    expect(profileResponse.statusCode).toEqual(200);
-    expect(profileResponse.body.user).toEqual(
-      expect.objectContaining({
-        email: 'johndoe@example.com',
-      }),
-    );
+    expect(response.statusCode).toEqual(201);
   });
 });
